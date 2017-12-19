c950179da8fd0ff268247ba8587e73067ef3a988
Borys Hulii <8tomat8@gmail.com>
2017-12-19 12:10:10 +0200

    Added changes from review

--- storage/controllers.go
+++ storage/controllers.go
@@ -1,25 +1,34 @@
 package storage
 
 import (
-	"io/ioutil"
-	"path/filepath"
-	"github.com/twinj/uuid"
 	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"path/filepath"
 	"strconv"
+
+	"flag"
+
+	"github.com/twinj/uuid"
+)
+
+var (
+	libPath = flag.String("libPath", "storage/storage.json", "Foo")
+
+	ErrNotFound = errors.New("can't find the book with given ID")
 )
 
 func writeData(books Books) error {
-	path, err := filepath.Abs("storage/storage.json")
+	path, err := filepath.Abs(*libPath)
 	if err != nil {
 		return err
 	}
+
 	booksBytes, err := json.MarshalIndent(books, "", "    ")
 	if err != nil {
 		return err
 	}
-	err = ioutil.WriteFile(path, booksBytes, 0644)
-	return err
+	return ioutil.WriteFile(path, booksBytes, 0644)
 }
 
 func wantedIndex(id string, books Books) (int, error) {
@@ -28,26 +37,24 @@ func wantedIndex(id string, books Books) (int, error) {
 			return index, nil
 		}
 	}
-	err := errors.New("can't find the book with given ID")
-	return 0, err
+	return 0, ErrNotFound
 }
 
 //GetBooks returns all book objects
 func GetBooks() (Books, error) {
 	var books Books
-	path, err := filepath.Abs("storage/storage.json")
+
+	path, err := filepath.Abs(*libPath)
 	if err != nil {
 		return nil, err
 	}
+
 	file, err := ioutil.ReadFile(path)
-    	if err != nil {
-		return nil, err
-    	}
-	err = json.Unmarshal(file, &books)
 	if err != nil {
 		return nil, err
 	}
-	return books, nil
+
+	return books, json.Unmarshal(file, &books)
 }
 
 // CreateBook adds book object into db
@@ -62,19 +69,17 @@ func CreateBook(book Book) error {
 		return err
 	case book.Title == "":
 		return err
-	default:
-		books, err := GetBooks()
-		if err != nil {
-			return err
-		}
-		book.ID = uuid.NewV4().String()
-		books = append(books, book)
-		err = writeData(books)
-		if err != nil {
-			return err
-		}
-		return nil
 	}
+
+	books, err := GetBooks()
+	if err != nil {
+		return err
+	}
+
+	book.ID = uuid.NewV4().String()
+	books = append(books, book)
+	return writeData(books)
+
 }
 
 // GetBook returns book object with specified id
@@ -84,13 +89,13 @@ func GetBook(id string) (Book, error) {
 	if err != nil {
 		return b, err
 	}
+
 	for _, book := range books {
 		if id == book.ID {
 			return book, nil
 		}
 	}
-	err  = errors.New("can't find the book with given ID")
-	return b, err
+	return b, ErrNotFound
 }
 
 // RemoveBook removes book object with specified id
@@ -99,13 +104,13 @@ func RemoveBook(id string) error {
 	if err != nil {
 		return err
 	}
+
 	index, err := wantedIndex(id, books)
 	if err != nil {
 		return err
 	}
 	books = append(books[:index], books[index+1:]...)
-	err = writeData(books)
-	return err
+	return writeData(books)
 }
 
 // ChangeBook updates book object with specified id
@@ -114,11 +119,13 @@ func ChangeBook(id string, changedBook Book) error {
 	if err != nil {
 		return err
 	}
+
 	index, err := wantedIndex(id, books)
 	if err != nil {
 		return err
 	}
-	book := &books[index]
+
+	book := books[index]
 	if changedBook.Price != 0 {
 		book.Price = changedBook.Price
 	}
@@ -138,19 +145,26 @@ func ChangeBook(id string, changedBook Book) error {
 // PriceFilter returns filtered book objects
 func PriceFilter(filter Filter) (Books, error) {
 	var wantedBooks Books
+
+	if len(filter.Price) <= 1 {
+		return nil, errors.New("Not valid data")
+	}
 	operator := string(filter.Price[0])
 	if operator != "<" && operator != ">" {
 		err := errors.New("unsupported operation")
 		return nil, err
 	}
+
 	books, err := GetBooks()
 	if err != nil {
 		return nil, err
 	}
+
 	price, err := strconv.ParseFloat(filter.Price[1:], 64)
 	if err != nil {
 		return nil, err
 	}
+
 	for _, book := range books {
 		if operator == ">" {
 			if book.Price > price {
--- storage/models.go
+++ storage/models.go
@@ -2,11 +2,11 @@ package storage
 
 // Book describes main data structure in the app
 type Book struct {
-	ID	string 		`json:"id, omitempty"`
-	Title	string		`json:"title, omitempty"`
-	Genres	[]string	`json:"genres, omitempty"`
-	Pages	int		`json:"pages, omitempty"`
-	Price	float64		`json:"price, omitempty"`
+	ID     string   `json:"id, omitempty"`
+	Title  string   `json:"title, omitempty"`
+	Genres []string `json:"genres, omitempty"`
+	Pages  int      `json:"pages, omitempty"`
+	Price  float64  `json:"price, omitempty"`
 }
 
 // Books contains book objects
@@ -14,5 +14,5 @@ type Books []Book
 
 //Filter describes filter indicator
 type Filter struct {
-	Price	string		`json:"price, omitempty"`
+	Price string `json:"price, omitempty"`
 }
