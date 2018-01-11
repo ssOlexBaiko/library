f9b3839f46dcaaa7daa195cc4165993ef6a16923
Borys Hulii <8tomat8@gmail.com>
2018-01-11 23:23:40 +0200

    Added review changes

--- main.go
+++ main.go
@@ -5,56 +5,36 @@ import (
 	"net/http"
 
 	"flag"
-	"os"
-	"path/filepath"
 
-	"github.com/jinzhu/gorm"
 	"github.com/ssOlexBaiko/library/api/web"
 	"github.com/ssOlexBaiko/library/storage"
 )
 
 var (
-	libPath = flag.String("libPath", "storage/storage.json", "set path the storage file")
-	useSql  = flag.Bool("useSql", false, "use sql db instead of json file")
+	fileLibPath = flag.String("libPath", "storage/storage.json", "set path the storage file")
+	sqlLibPath  = flag.String("libPath", "storage/data.db", "set path the storage file")
 )
 
 func main() {
 	var (
-		sqlStorage *gorm.DB
-		path       string
-		err        error
-		store      web.Storage
+		err   error
+		store web.Storage
 	)
 
-	defer func() {
-		if sqlStorage != nil {
-			err := sqlStorage.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}()
-
-	path, err = filepath.Abs(*libPath)
+	flag.Parse()
+	if len(*sqlLibPath) != 0 {
+		store, err = storage.NewSQLLibrary(*sqlLibPath)
+	} else {
+		store, err = storage.NewLibrary(*fileLibPath)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	flag.Parse()
-	if *useSql {
-		sqlStorage, err = storage.InitDB()
-		if err != nil {
+	defer func() {
+		if err := store.Close(); err != nil {
 			log.Fatal(err)
 		}
-
-		store = storage.NewSQLLibrary(sqlStorage)
-	} else {
-		file, err := os.OpenFile(path, os.O_RDWR, 0660)
-		defer file.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-		store = storage.NewLibrary(file)
-	}
+	}()
 
 	router := web.NewRouter(
 		web.NewHandler(store),
